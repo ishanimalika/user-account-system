7f5ba467eee9d2eb413d03e6382e441c31bb8775
ishanimalika <ishanimalika21@gmail.com>
2025-09-24 15:32:21 +0530

    create frontend pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from "./components/home/Home"
+import { Route,Routes } from 'react-router-dom';
+import User from "./components/AddUser/User"
+import Users from "./components/UserDetails/Users"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+        <React.Fragment>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/mainhome" element={<Home />}/>
+            <Route path="/adduser" element={<User />}/>
+            <Route path="/userdetails" element={<Users />}/>
+          </Routes>
+        </React.Fragment>
+        
     </div>
   );
 }
