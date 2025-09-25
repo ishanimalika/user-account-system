9fd1cb94d2ebda9ceb43cc71301962885e31ad6c
ishanimalika <ishanimalika21@gmail.com>
2025-09-25 16:52:46 +0530

    implement search function

--- src/components/UserDetails/Users.js
+++ src/components/UserDetails/Users.js
@@ -22,12 +22,42 @@ function Users() {
       contentRef: ComponentRef,
       DocumentTitle:"Users Report",
       onafterprint:()=>alert("Users Report Successfully Download !"),
-  })
+  });
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [noResults, setNoResults] = useState(false);
+
+  const handleSearch = () => {
+    fetchHandler().then((data) =>{
+      const filteredUsers = data.user.filter((user) =>
+      Object.values(user).some((field)=>
+      field.toString().toLowerCase().includes(searchQuery.toLowerCase())
+      ))
+      setUsers(filteredUsers);
+      setNoResults(filteredUsers.length === 0);
+    })
+  }
 
   return (
     <div>
       <Nav/>
       <h1>User Details Display Page</h1>
+      <input onChange={(e) => setSearchQuery(e.target.value)}
+         type="text"
+         name="search"
+         placeholder="Search User Details"
+      ></input>
+
+      <button onClick={handleSearch}>Search</button>
+
+      {noResults ? (
+
+        <div>
+          <p>No Users Found</p>
+        </div>
+
+      ):(
+
       <div ref={ComponentRef}>
         {users && users.map((user, i) => (
           <div key ={i}>
@@ -35,6 +65,8 @@ function Users() {
           </div>
         ))}
       </div>
+      )}
+
       <br/>
       <button onClick={handlePrint}>Download Report</button>
     </div>
